bffbd5f02ea6d18b53b93275704ed940ea091e6e
ahmetulutas <englishlectures.stocks@gmail.com>
2022-03-30 01:50:40 +0300

    added dnd to the lists also

--- src/components/Boards/SingleBoard.tsx
+++ src/components/Boards/SingleBoard.tsx
@@ -63,11 +63,6 @@ export default function SingleBoard() {
     const destinationList = lists.find((list:any) => list.id === Number(destination.droppableId));
     const sourceCards = [...sourceList.cards];
     const destinationCards = [...destinationList.cards];
-    let followingCard;
-    let prevCard;
-    let orderValue = 0;
-
-    console.log("destination index is: ", destination.index);
     // returns if item drops in a non-draggable area.
     if (!destination) return;
 /*  ------------------ if the item is dropped in the different list. ----------------- */
@@ -77,80 +72,15 @@ export default function SingleBoard() {
       destinationCards.forEach((card:any, index:number) => {
         dispatch(updateCard({id:card.id, order:index, listId:Number(destination.droppableId)}));
       })
-      /* if (destination.index === 0) {
-        console.log("adding to the top in different list")
-        followingCard = destinationCards[destination.index +1];
-        console.log("followingCard is",followingCard);
-        console.log("destinationCards is",destinationCards);
-        console.log("removedCard is",removedCard);
-        followingCard ? orderValue = (followingCard.order / 2) : orderValue = removedCard.order;
-        console.log("orderValue is", orderValue)
-        dispatch(updateCard({id:removedCard.id, order:orderValue, listId:Number(destination.droppableId)}));
-      }
-      else if (destination.index === destinationCards.length-1) {
-        console.log(destinationCards.length)
-        console.log("adding to the end in different list" )
-        prevCard = destinationCards[destination.index -1];
-        console.log("destinationCards is",destinationCards);
-        console.log("destinationIndex is",destination.index);
-        console.log("prevCard is",prevCard);
-        console.log("removedCard is",removedCard);
-        prevCard ? orderValue = (prevCard.order * 2) : orderValue = removedCard.order;
-        dispatch(updateCard({id:removedCard.id, order:orderValue, listId:Number(destination.droppableId)}));
-      }
-      else {
-        console.log("adding to the inbetween in different list")
-        prevCard = destinationCards[destination.index - 1];
-        followingCard = destinationCards[destination.index +1];
-        if (prevCard === removedCard) { 
-          prevCard = destinationCards[destination.index];
-          followingCard = destinationCards[destination.index + 1];
-        } 
-        orderValue = (prevCard.order + followingCard.order)/2;
-        console.log("prevCard is",prevCard, "followingCard is", followingCard, "orderValue is", orderValue);      
-        dispatch(updateCard({id:removedCard.id, order:orderValue, listId:Number(destination.droppableId)}));
-      } */
-        dispatch(updateListWithDnd({sourceId:Number(source.droppableId), destinationId:Number(destination.droppableId), sourceCards, destinationCards, draggableId}));
+      dispatch(updateListWithDnd({sourceId:Number(source.droppableId), destinationId:Number(destination.droppableId), sourceCards, destinationCards, draggableId}));
       } 
 /* ---------------------------- Same List Condition --------------------- */ 
       else {
         const [removedCard] = sourceCards.splice(source.index, 1);
         sourceCards.splice(destination.index, 0, removedCard);
-        console.log("sourceCards", sourceCards)
-        console.log("destinationCards", destinationCards)
         sourceCards.forEach((card:any, index:number) => {
-            dispatch(updateCard({id:card.id, order:index, listId:Number(destination.droppableId)}));
+          dispatch(updateCard({id:card.id, order:index, listId:Number(destination.droppableId)}));
         })
-        /* if(destination.index === 0) {
-          console.log("adding to the top in the same list");
-          followingCard = sourceCards[destination.index +1];
-          orderValue = (followingCard.order + 0)/2;
-          dispatch(updateCard({id:removedCard.id, order:orderValue, listId:Number(source.droppableId)}));
-        }
-        if (destination.index === sourceCards.length - 1) {
-          console.log("adding to the end in the same list");
-          prevCard = sourceCards[destination.index-1];   
-          orderValue = (prevCard.order *2);;
-          dispatch(updateCard({id:removedCard.id, order:orderValue, listId:Number(source.droppableId)}));
-        }
-        else {
-          console.log("adding to the inbetween in the same list")
-          prevCard = destinationCards[destination.index - 1];
-          followingCard = destinationCards[destination.index +1];
-          if(prevCard === removedCard) { 
-            // if prevcard is the same as removed card, then prevCard is the card the card after the removed card in initial position.
-            prevCard = destinationCards[destination.index];
-            followingCard = destinationCards[destination.index + 1];
-          } 
-          if(followingCard === removedCard) {
-            // if followingCard is the same as removed card, then followingCard is the card before the removed card in initial position.
-            followingCard = destinationCards[destination.index];
-            prevCard = destinationCards[destination.index - 1];
-          }
-          orderValue = (prevCard.order + followingCard.order)/2;
-          console.log("prevCard is",prevCard, "followingCard is", followingCard, "orderValue is", orderValue);      
-          dispatch(updateCard({id:removedCard.id, order:orderValue, listId:Number(source.droppableId)}));
-        } */
         dispatch(updateListWithDnd({sourceId:Number(source.droppableId), destinationId:Number(destination.droppableId), sourceCards, destinationCards, draggableId}));
       }
 }
