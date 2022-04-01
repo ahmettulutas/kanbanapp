e82f0e218465e50884ec39b98934913c48e4974f
ahmetulutas <englishlectures.stocks@gmail.com>
2022-04-01 03:42:06 +0300

    solved some ui issues

--- src/components/List/ListComponent.tsx
+++ src/components/List/ListComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Box } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import Card from '../Card/Card';
